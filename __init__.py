4e395a9fabe2fdb2afc21505a87cfe98c9bfe248
Stuart Wheaton <stuart@voxel51.com>
2023-11-04 21:36:02 -0400

    remove running of owlvit; blur existing detection boxes instead

--- __init__.py
+++ __init__.py
@@ -5,10 +5,10 @@ I/O operators.
 | `voxel51.com <https://voxel51.com/>`_
 |
 """
+import functools
 import os
 
 import cv2
-import numpy as np
 from PIL import Image
 
 import fiftyone as fo
@@ -17,167 +17,85 @@ import fiftyone.operators.types as types
 import fiftyone.core.utils as fou
 
 
-def _set_progress(ctx, progress, label=None, delegated=False):
-    if delegated:
-        return None
-    return ctx.trigger("set_progress", dict(progress=progress, label=label))
+def stack_blur(roi):
+    """Perform stack blur. Faster than gaussian, looks better than pixelation"""
+    #   stackBlur() can segfault if ksize is too big relative
+    #   to the roi so cap it to a smaller amount.
+    ksize = min(101, min(roi.shape[0:2]) * 2 + 1)
+    roi = cv2.stackBlur(roi, ksize=(ksize, ksize))
+    return roi
 
 
-def process_image_results(
-    image, image_results, query_string, output_dir, store_detections
+def pixelate(roi):
+    """Pixelation approach"""
+    height, width, _ = roi.shape
+    roi = cv2.resize(roi, (16, 16), interpolation=cv2.INTER_LINEAR)
+    roi = cv2.resize(roi, (width, height), interpolation=cv2.INTER_NEAREST)
+    return roi
+
+
+def gaussian_blur(roi):
+    """Use gaussian blur to blur"""
+    # Gaussian blur approach
+    roi = cv2.GaussianBlur(roi, (51, 51), 0)
+    return roi
+
+
+def anonymize_media(
+    target_view,
+    output_dir,
+    media_path,
+    detections_field,
+    blur_method,
+    progress_callback,
 ):
-    file_path = image.filename
-    detections = []
-    if image_results["boxes"].any():
-        # Use NP array in BGR, which cv2 likes
-        cv2_image = np.asarray(image)
-        cv2_image = cv2.cvtColor(cv2_image, cv2.COLOR_RGB2BGR)
-        for box, score, label_num in zip(
-            image_results["boxes"],
-            image_results["scores"],
-            image_results["labels"],
-        ):
-            x0, y0, x1, y1 = box.tolist()
-            slice_y, slice_x = slice(
-                int(y0 * image.height), int(y1 * image.height)
-            ), slice(int(x0 * image.width), int(x1 * image.width))
+    anon_file_paths = {}
+    filepaths, boxes_arr = target_view.values(
+        ("filepath", f"{detections_field}.detections.bounding_box")
+    )
+    total = len(target_view)
+    for (i, (filepath, boxes)) in enumerate(zip(filepaths, boxes_arr)):
+
+        cv2_image = cv2.imread(filepath)
+
+        for box in boxes:
+            x0, y0, width, height = box
+            total_height, total_width, _ = cv2_image.shape
+
+            slice_y = slice(int(y0 * total_height), int((y0 + height) * total_height))
+            slice_x = slice(int(x0 * total_width), int((x0 + width) * total_width))
+
             roi = cv2_image[slice_y, slice_x]
 
             if roi.any():
-                # Gaussian blur approach
-                # roi = cv2.GaussianBlur(roi, (51, 51), 0)
-
-                # Perform stack blur. Faster than gaussian, looks better
-                #   than pixelation.
-                #   stackBlur() can segfault if ksize is too big relative
-                #   to the roi so cap it to a smaller amount.
-                ksize = min(101, min(roi.shape[0:2]) * 2 + 1)
-                roi = cv2.stackBlur(roi, ksize=(ksize, ksize))
-
-                # Pixelation approach
-                # height, width, _ = roi.shape
-                # roi = cv2.resize(roi, (16, 16), interpolation=cv2.INTER_LINEAR)
-                # roi = cv2.resize(roi, (width, height), interpolation=cv2.INTER_NEAREST)
+                if blur_method == "gaussian":
+                    roi = gaussian_blur(roi)
+                elif blur_method == "stack":
+                    roi = stack_blur(roi)
+                elif blur_method == "pixelate":
+                    roi = pixelate(roi)
+                else:
+                    raise ValueError(f"Unknown blur method '{blur_method}'")
 
                 # impose this blurred image on original image to get final image
                 cv2_image[slice_y, slice_x] = roi
-
-                # Create FO detection for this box
-                if store_detections:
-                    box_width = x1 - x0
-                    box_height = y1 - y0
-                    label = query_string
-                    if isinstance(query_string, list):
-                        label = query_string[label_num]
-                    detections.append(
-                        fo.Detection(
-                            label=label,
-                            confidence=score,
-                            bounding_box=[
-                                x0,
-                                y0,
-                                box_width,
-                                box_height,
-                            ],
-                        )
-                    )
 
         # Convert back to PIL.Image for saving
         cv2_image = cv2.cvtColor(cv2_image, cv2.COLOR_BGR2RGB)
         image = Image.fromarray(cv2_image)
 
-    base_name = os.path.basename(file_path)
-    root, ext = os.path.splitext(base_name)
-    anon_file_path = os.path.join(output_dir, f"{root}_anon{ext}")
-    image.save(anon_file_path)
+        base_name = os.path.basename(filepath)
+        root, ext = os.path.splitext(base_name)
+        anon_file_path = os.path.join(output_dir, f"{root}_anon{ext}")
+        image.save(anon_file_path)
+        anon_file_paths[filepath] = anon_file_path
 
-    return detections, anon_file_path
-
-
-def process_batch_results(
-    results, batch, query_string, output_dir, store_detections
-):
-    out_files = []
-    batch_detections = []
-    for i, image_results in enumerate(results):
-        detections, anon_file_path = process_image_results(
-            batch[i], image_results, query_string, output_dir, store_detections
+        yield progress_callback(
+            progress=i / total, label=f"{i}/{total} samples anonymized"
         )
-        if store_detections:
-            batch_detections.append(fo.Detections(detections=detections))
-        out_files.append(anon_file_path)
 
-    return out_files, batch_detections
-
-
-def anonymize_batch(
-    processor, model, batch, query_string, output_dir, store_detections
-):
-    inputs = processor(
-        text=[query_string] * len(batch), images=batch, return_tensors="pt"
-    )
-    # inputs.to(device)
-
-    outputs = model(**inputs)
-    # outputs.to(device)
-
-    # Convert outputs (bounding boxes and class logits) to COCO API
-    results = processor.post_process_object_detection(
-        outputs=outputs, threshold=0.05
-    )
-
-    return process_batch_results(
-        results, batch, query_string, output_dir, store_detections
-    )
-
-
-def anonymize_media(
-    ctx,
-    target_view,
-    query_string,
-    output_dir,
-    store_detections,
-    delegated,
-    media_path,
-):
-    from transformers import OwlViTProcessor, OwlViTForObjectDetection
-
-    # Comma separated queries
-    if "," in query_string:
-        query_string = [s.strip() for s in query_string.split(",")]
-
-    processor = OwlViTProcessor.from_pretrained("google/owlvit-base-patch32")
-    model = OwlViTForObjectDetection.from_pretrained(
-        "google/owlvit-base-patch32"
-    )
-    # device = torch.device("mps")
-    # processor.to(device)
-    # model.to(device)
-
-    file_paths = target_view.values("filepath")
-    images = [Image.open(f) for f in file_paths]
-
-    batch_size = 1
-    yield _set_progress(ctx, 0, f"Anonymized 0 of {len(images)}", delegated)
-
-    all_detections = []
-    new_files = []
-    for batch_start in range(0, len(images), batch_size):
-        batch = images[batch_start : batch_start + batch_size]
-        batch_files, batch_detections = anonymize_batch(
-            processor, model, batch, query_string, output_dir, store_detections
-        )
-        new_files += batch_files
-        all_detections += batch_detections
-
-        progress = (batch_start + batch_size) / len(images)
-        label = f"Anonymized {batch_start + batch_size} of {len(images)}"
-        yield _set_progress(ctx, progress, label, delegated)
-
-    if store_detections:
-        target_view.set_values("anonymized_detections", all_detections)
-
-    target_view.set_values(media_path, new_files)
+    target_view.set_values(media_path, anon_file_paths, key_field="filepath")
+    yield progress_callback(progress=1, label="All samples anonymized")
 
 
 def get_target_view(ctx, inputs):
@@ -269,7 +187,7 @@ class AnonymizeMedia(foo.Operator):
     def config(self):
         return foo.OperatorConfig(
             name="blur_objects",
-            label="Blur Objects in Images",
+            label="Anonymize/Blur Objects in Images",
             dynamic=True,
             execute_as_generator=True,
         )
@@ -281,51 +199,66 @@ class AnonymizeMedia(foo.Operator):
         if ready:
             _execution_mode(ctx, inputs)
 
-        return types.Property(
-            inputs, view=types.View(label="Anonymize Images")
-        )
+        return types.Property(inputs, view=types.View(label="Anonymize Images"))
 
     def resolve_delegation(self, ctx):
         return ctx.params.get("delegate", False)
 
-    def execute(self, ctx):
+    async def execute(self, ctx):
         # Params
         target = ctx.params.get("target", None)
-        query_string = ctx.params["query_string"]
-        media_path = ctx.params["media_path"]
+        detections_field = ctx.params["detections_field"]
+        mode = ctx.params.get("mode")
         output_dir = ctx.params["output_dir"]["absolute_path"]
         overwrite = ctx.params.get("overwrite", False)
-        store_detections = ctx.params.get("store_detections", True)
         delegated = ctx.params.get("delegated", False)
         target_view = _get_target_view(ctx, target)
 
-        if not overwrite:
-            target_view = target_view.exists(media_path, False)
+        update_ctx_dataset = False
+        if mode == "alt":
+            media_path = ctx.params["media_path"]
+            update_ctx_dataset = True
+            if not overwrite:
+                target_view = target_view.exists(media_path, False)
+        elif mode == "clone":
+            clone_dataset = ctx.params["clone_dataset"]
+            target_view = target_view.clone(clone_dataset)
+            media_path = "filepath"
+        else:
+            raise ValueError("Invalid mode")
+
+        def _set_progress(ctx, progress, label=None, delegated=False):
+            if delegated:
+                return None
+            return ctx.trigger("set_progress", dict(progress=progress, label=label))
 
         num_total = len(target_view)
+        progress_callback = functools.partial(
+            _set_progress, ctx=ctx, delegated=delegated
+        )
         with fou.ProgressBar(total=num_total) as pb:
             for update in pb(
                 anonymize_media(
-                    ctx,
                     target_view,
-                    query_string,
                     output_dir,
-                    store_detections,
-                    delegated,
                     media_path,
+                    detections_field,
+                    "stack",
+                    progress_callback,
                 )
             ):
                 yield update
 
         # Update media fields
-        if media_path not in ctx.dataset.app_config.media_fields:
-            ctx.dataset.app_config.media_fields.append(media_path)
-        ctx.dataset.app_config.grid_media_field = media_path
-        ctx.dataset.app_config.modal_media_field = media_path
+        if update_ctx_dataset:
+            if media_path not in ctx.dataset.app_config.media_fields:
+                ctx.dataset.app_config.media_fields.append(media_path)
+            ctx.dataset.app_config.grid_media_field = media_path
+            ctx.dataset.app_config.modal_media_field = media_path
 
-        # Save and reload
-        ctx.dataset.save()
-        yield ctx.trigger("reload_dataset")
+            # Save and reload
+            ctx.dataset.save()
+            yield ctx.trigger("reload_dataset")
 
 
 def _parse_path(ctx, key):
@@ -334,6 +267,12 @@ def _parse_path(ctx, key):
 
 
 def _anonymize_media_inputs(ctx, inputs):
+    if ctx.dataset.media_type != "image":
+        label = "Only image datasets currently supported."
+        status1 = inputs.view("status1", types.Warning(label=label))
+        status1.invalid = True
+        return False
+
     # Dataset, view, or selection?
     target_view = get_target_view(ctx, inputs)
 
@@ -345,46 +284,83 @@ def _anonymize_media_inputs(ctx, inputs):
     else:
         target_str = "dataset"
 
-    # Query string
-    query_string_selector = types.AutocompleteView()
-    for preset in ["human face", "person", "license plate"]:
-        query_string_selector.add_choice(preset, label=preset.title())
+    detection_field_selector = types.AutocompleteView()
+    for field in target_view.get_field_schema(embedded_doc_type=fo.Detections):
+        detection_field_selector.add_choice(field, label=field)
+
     inputs.str(
-        "query_string",
+        "detections_field",
         required=True,
-        label="Query String",
-        description="Text query string to search for, can be a word or phrase. Pass a comma ',' separated list to search for multiple classes; e.g., horse,cat,dog",
-        view=query_string_selector,
+        label="Detections field",
+        description=(
+            "Choose an existing Detections field to use for anonymization "
+            "bounding boxes."
+        ),
+        view=detection_field_selector,
     )
 
-    query_string = ctx.params.get("query_string", None)
-    if not query_string:
+    detections_field = ctx.params.get("detections_field", None)
+
+    if detections_field is None:
         return False
 
-    field_selector = types.AutocompleteView()
-    field_selector.add_choice(
-        "anonymized_filepath", label="anonymized_filepath"
-    )
-    for field in target_view.get_field_schema(ftype=fo.StringField):
-        if field == "filepath":
-            continue
-
-        field_selector.add_choice(field, label=field)
-
+    mode_selector = types.Choices()
+    mode_selector.add_choice("alt", label="Create alternative media source")
+    mode_selector.add_choice("clone", label="Clone into new dataset")
     inputs.str(
-        "media_path",
+        "mode",
         required=True,
-        label="Anonymized media field",
-        description=(
-            "Provide the name of a new or existing field in which to "
-            "store the anonymized media paths"
-        ),
-        view=field_selector,
+        label="",
+        description="",
+        default="alt",
+        view=mode_selector,
     )
 
-    media_path = ctx.params.get("media_path", None)
+    mode = ctx.params.get("mode")
+    if not mode:
+        return False
+    if mode == "alt":
+        field_selector = types.AutocompleteView()
+        field_selector.add_choice("anonymized_filepath", label="anonymized_filepath")
+        for field in target_view.get_field_schema(ftype=fo.StringField):
+            if field in {"anonymized_filepath", "filepath"}:
+                continue
 
-    if media_path is None:
+            field_selector.add_choice(field, label=field)
+
+        inputs.str(
+            "media_path",
+            required=True,
+            label="Anonymized media field",
+            description=(
+                "Provide the name of a new or existing field in which to "
+                "store the anonymized media paths"
+            ),
+            view=field_selector,
+        )
+
+        media_path = ctx.params.get("media_path", None)
+
+        if media_path is None:
+            return False
+
+        inputs.bool(
+            "overwrite",
+            default=False,
+            label="Regenerate anonymized media?",
+            description=(
+                "Whether to regenerate anonymized media for samples that "
+                f"already have their '{media_path}' field populated"
+            ),
+            view=types.SwitchView(),
+        )
+
+    elif mode == "clone":
+        inputs.str("clone_dataset", required=True, label="Clone dataset name")
+
+        if "clone_dataset" not in ctx.params:
+            return False
+    else:
         return False
 
     file_explorer = types.FileExplorerView(
@@ -403,28 +379,12 @@ def _anonymize_media_inputs(ctx, inputs):
     )
     output_dir = _parse_path(ctx, "output_dir")
 
-    if output_dir is None:
+    if not output_dir:
         return False
-
-    inputs.bool(
-        "store_detections",
-        default=True,
-        label="Store detections used for anonymization?",
-        description="Whether to store output detections from this model run as a field on the dataset",
-        view=types.SwitchView(),
-    )
-
-    inputs.bool(
-        "overwrite",
-        default=False,
-        label="Regenerate anonymized media?",
-        description=f"Whether to regenerate anonymized media for samples that already have their '{media_path} field populated'",
-        view=types.SwitchView(),
-    )
 
     overwrite = ctx.params.get("overwrite", False)
 
-    if overwrite:
+    if overwrite or mode == "clone":
         n = len(target_view)
         if n > 0:
             label = f"Found {n} samples to (re)generate anonymized media for"
